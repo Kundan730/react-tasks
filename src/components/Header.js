f80b265c97a6bf52c06ca5742fb5cc700eb8bf32
Kundan Kumar Sahu <espkundan@gmail.com>
2023-04-01 12:26:02 +0530

    Initial Commit

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import Button from './Button'
+
+const Header = ({ title, onAdd, showAdd }) => {
+
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      < Button onClick={onAdd} color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} />
+    </header>
+  )
+}
+
+Header.defaultProps = {
+  title: 'Test'
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired
+}
+
+//CSS in JSX
+// const headingStyle = {
+//   backgroundColor: 'yellow',
+//   color: 'blue'
+// }
+
+
+export default Header
